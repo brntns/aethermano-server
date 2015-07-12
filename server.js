0dcf4d31c6b25234cb5767229db1f4d30ca7b608
Jannis Reinsch <jannis@brntns.com>
2015-07-12 16:41:12 +0200

    further monster sync NPM INSTALL before starting server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = require('http').createServer()
 	, _ = require('lodash')
 	, gameMap = require('./map.js')
   , gameItems = require('./items.js')
-	, gameMonster = require('./monster.js')
+	, gameMonster = require('./monster.js');
+var Infiniteloop = require('infinite-loop');
 
 var map = new gameMap.Map();
 var items = new gameItems.Items();
@@ -18,8 +19,7 @@ var monsterNum = monsterPerScreen*map.mapSize/3072;
 //for (i = 0; i < monsterNum; i++) {
 	monster.create();
 //}
-//	monster.create();
-console.log(monster.monsters);
+//console.log(monster.monsters);
 items.create();
 //app.listen(process.env.PORT);
 app.listen(8000);
@@ -45,23 +45,23 @@ io.sockets.on('connection', function (socket) {
 	socket.emit('getMap', map.maps, items.itemData);
 	//update player
   socket.broadcast.to('level1').emit('updatePlayers', [player])
-
 	// update Spawnpoints
 	socket.on('mapCreated', function(){
 		socket.emit('playerSpawn', spawnPoint);
-		//socket.emit('monsterSpawns', monsterSpawns);
 		// send Monster
-		socket.emit('updateMonsters',monster.monsters)
+		socket.emit('updateMonsters',monster.monsters);
 	});
-
 	// update player postition
 	socket.on('newPlayerPosition', function (data) {
 		player.x = data.x;
 		player.y = data.y;
 		player.status = data.status;
     player.level = data.level;
-    //console.log(data.level)
     socket.broadcast.to(data.level).emit('updatePlayers', [player]);
+	});
+	socket.on('monsterSet', function(monster){
+		// send Monster
+		startMovement(monster);
 	});
 	//update monsters
 	socket.on('monsterUpdate', function (data) {
@@ -71,7 +71,6 @@ io.sockets.on('connection', function (socket) {
 	//update level
   socket.on('requestLevelChange', function (level) {
 		// console.log(level);
-		// console.log(map.maps.length);
 		if (map.maps.length <= level+1) {
 			map.create();
 		}
@@ -79,14 +78,14 @@ io.sockets.on('connection', function (socket) {
     socket.join(level + 1);
     socket.room = level + 1;
   	socket.emit('changeLevel', {level:socket.room, map:map.maps});
-  	});
-  	socket.on('mapUpdated', function(){
+  });
+  socket.on('mapUpdated', function(){
     console.log('mapupdated');
     var spawnx = Math.random()*1600*16;
     var spawny = Math.random()*1600*16;
     var respawnPoint = {x: spawnx, y: spawny};
     socket.emit('playerRepawn', respawnPoint);
-  	});
+  });
 	//disctonnect
 	socket.on('disconnect', function () {
 		_.remove(players, function(p) {
@@ -95,38 +94,47 @@ io.sockets.on('connection', function (socket) {
 		socket.broadcast.emit('removePlayer', player.id);
 	});
 });
-
+// Monster Movement Loops
+function monsterMoveRight(monster){
+		monster.x++;
+		io.sockets.emit('updateMonsters', [monster])
+		console.log(monster);
+}
+function startMovement(monster){
+	var loop = new Infiniteloop();
+	loop.add(monsterMoveRight,monster);
+	loop.setInterval(150).run();
+}
 //block
 var fs = require('fs'),
 PNG = require('pngjs').PNG;
 
 function writeImg() {
-    var img = new PNG({
-      filterType: 4,
-      width: 1600,
-      height: 1600
-    });
-    for (var y = 0; y < img.height; y++) {
-      for (var x = 0; x < img.width; x++) {
-        var idx = (img.width * y + x) << 2;
-        // invert color
-        if (map.map[x+1600*y] == 0){
-          img.data[idx] = 255;
-          img.data[idx+1] = 255;
-          img.data[idx+2] = 255;
-          // and reduce opacity
-          img.data[idx+3] = 255;
-        } else {
-          img.data[idx] = 0;
-          img.data[idx+1] = 0;
-          img.data[idx+2] = 0;
-          // and reduce opacity
-          img.data[idx+3] = 255;
-        }
+  var img = new PNG({
+    filterType: 4,
+    width: 1600,
+    height: 1600
+  });
+  for (var y = 0; y < img.height; y++) {
+    for (var x = 0; x < img.width; x++) {
+      var idx = (img.width * y + x) << 2;
+      // invert color
+      if (map.map[x+1600*y] == 0){
+        img.data[idx] = 255;
+        img.data[idx+1] = 255;
+        img.data[idx+2] = 255;
+        // and reduce opacity
+        img.data[idx+3] = 255;
+      } else {
+        img.data[idx] = 0;
+        img.data[idx+1] = 0;
+        img.data[idx+2] = 0;
+        // and reduce opacity
+        img.data[idx+3] = 255;
       }
     }
-
-    img.pack().pipe(fs.createWriteStream('out.png'));
+  }
+  img.pack().pipe(fs.createWriteStream('out.png'));
 }
 
  writeImg();
